77c15042e51a90359fdf4c7e6e9c4ceec67fb20b
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-01-17 06:40:57 +0300

    Completed Lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_numbers=[]
+    for i in range(length):
+        if i == 0:
+            fib_numbers.append(0)
+        elif i == 1:
+            fib_numbers.append(1)
+        else:
+            next_number = fib_numbers[-1] + fib_numbers[-2]
+            fib_numbers.append(next_number)
+    print(fib_numbers)
+
+print_fibonacci(10)
